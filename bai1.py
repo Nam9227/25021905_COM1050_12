37a9201ac149a263bc025cf6d7be2e52023421c0
Nam9227 <vn5753378@gmail.com>
2025-10-01 18:39:51 +0700

    Update bai1.py

--- bai1.py
+++ bai1.py
@@ -1,1 +1,7 @@
-print("chua co ny")
+a,b,c = map(int,input().split())
+if a > b and a>c : 
+    print (a, " la so lơn nhat ")
+elif b> c:
+    print(b, "la so lon nhat")
+else:
+    print(c, "la so lon nhat")
