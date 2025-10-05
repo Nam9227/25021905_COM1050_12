917a62a31e750d57780ce5674c2ef9aabe3cbb74
Nam9227 <vn5753378@gmail.com>
2025-10-05 20:24:56 +0700

    Create W2A20.py

---
+++ NguyenVanNam_COM1050_12_Lab01/W2A20.py
@@ -0,0 +1,18 @@
+a=[
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+    ]
+for i in a:
+    print(i)
+
+
